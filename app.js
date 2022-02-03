dfa5f9b66e716b75901b69c8538033c2d22194b3
Xemil85 <heiskaneneemil@gmail.com>
2022-02-03 12:20:29 +0200

    commit

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
-const config = require("./utils/config");
-const express = require("express");
+const config = require('./utils/config')
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
 require('express-async-errors')
-const app = express();
-const cors = require("cors");
-const notesRouter = require("./controllers/notes");
-const middleware = require("./utils/middleware");
-const logger = require("./utils/logger");
-const mongoose = require("mongoose");
+const cors = require('cors')
+const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
 
-logger.info("connecting to", config.MONGODB_URI);
+logger.info('connecting to', config.MONGODB_URI)
 
-mongoose
-  .connect(config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(() => {
-    logger.info("connected to MongoDB");
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    logger.error("error connection to MongoDB:", error.message);
-  });
+    logger.error('error connection to MongoDB:', error.message)
+  })
 
 app.use(cors())
 app.use(express.static('build'))
@@ -25,6 +26,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
